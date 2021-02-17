6188e3adc2a5bc8c600fca9f0786f83b8effb2b1
David Romero <romeroad@sheridancollage.ca>
2021-02-16 19:34:30 -0500

    Initial commit

---
+++ src/test/java/sheridan/romeroad/petdata/PetDataApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.romeroad.petdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
