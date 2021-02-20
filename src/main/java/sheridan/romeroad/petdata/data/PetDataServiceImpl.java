98f39ea9ac0f8efcb905306bcba177176f96cee5
David Romero <romeroad@sheridancollage.ca>
2021-02-19 19:59:39 -0500

    completed PetDataServiceImpl.java

--- src/main/java/sheridan/romeroad/petdata/data/PetDataServiceImpl.java
+++ src/main/java/sheridan/romeroad/petdata/data/PetDataServiceImpl.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Service;
 import sheridan.romeroad.petdata.model.PetEntity;
 import sheridan.romeroad.petdata.model.PetForm;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public abstract class PetDataServiceImpl implements PetDataService{
     private final PetDataRepo petDataRepo;
@@ -27,5 +31,50 @@ public abstract class PetDataServiceImpl implements PetDataService{
         form.setPetGender(pet.getPetGender());
         form.setPetVaccination(pet.isPetVaccinated());
     }
+
+    public void insertPetForm(PetForm form){
+        PetEntity pet = new PetEntity();
+        copyFormToEntity(form, pet);
+        pet = petDataRepo.save(pet);
+        form.setId(pet.getId());
+    }
+
+    public List<PetForm> getAllPetForms(){
+        List<PetForm> formList = new ArrayList<>();
+        List<PetEntity> petList = petDataRepo.findAll();
+        for(PetEntity pet: petList){
+            PetForm form = new PetForm();
+            copyEntityToForm(pet, form);
+            formList.add(form);
+        }
+        return formList;
+    }
+    public void deleteAllPetForms(){
+        petDataRepo.deleteAll();
+    }
+    public void deletePetForm(int id){
+        petDataRepo.deleteById(id);
+    }
+    public PetForm getPetForm(int id){
+        Optional<PetEntity> result = petDataRepo.findById(id);
+        if(result.isPresent()){
+            PetForm form = new PetForm();
+            PetEntity pet = result.get();
+            copyEntityToForm(pet, form);
+            return form;
+        }
+        return null;
+    }
+
+    public void updatePetForm(PetForm form){
+        Optional<PetEntity> result = petDataRepo.findById(form.getId());
+        if(result.isPresent()){
+            PetEntity pet = result.get();
+            copyFormToEntity(form,pet);
+            petDataRepo.save(pet);
+            petDataRepo.flush();
+        }
+    }
+
 
 }
